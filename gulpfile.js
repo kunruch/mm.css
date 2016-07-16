5d0e350d5832c41e5e6a7e2d0570c5b19037e3af
Kanishk Kunal <kanishk.kunal@hotmail.com>
2016-07-16 23:50:40 +0530

    Adding gulp task for CSS minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 // Sass configuration
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('sass', function() {
     gulp.src('./src/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('./dist/'))
 });
+
+gulp.task('min', ['sass'], function() {
+    gulp.src('./dist/*.css')
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('./dist/'))
+})
 
 gulp.task('default', ['sass'], function() {
     gulp.watch('src/**/*', ['sass']);
