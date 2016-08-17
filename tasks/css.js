7ccc529cb29f5496554af4fe9f8514dac61fa075
Kanishk Kunal <kanishk.kunal@hotmail.com>
2016-08-17 21:58:16 +0530

    Adding CSSComb for prettier CSS output

--- tasks/css.js
+++ tasks/css.js
@@ -3,6 +3,7 @@ var gulp    = require('gulp');
 var sass = require('gulp-sass');
 var prefix    = require('gulp-autoprefixer');
 var cleanCSS = require('gulp-clean-css');
+var csscomb = require('gulp-csscomb');
 var rename = require('gulp-rename');
 
 /**
@@ -12,7 +13,9 @@ gulp.task('css', function () {
     return gulp.src(config.css.SRC)
         .pipe(sass({ includePaths: config.css.INCLUDE_PATHS }).on('error', sass.logError))
         .pipe(prefix('last 2 versions', { cascade: true }))
+        .pipe(csscomb())
         .pipe(gulp.dest(config.css.DEST)) //non-minified CSS
+        
 });
 
 /**
@@ -22,6 +25,7 @@ gulp.task('css-min', function () {
     return gulp.src(config.css.SRC)
         .pipe(sass({ includePaths: config.css.INCLUDE_PATHS }).on('error', sass.logError))
         .pipe(prefix('last 2 versions', { cascade: true }))
+        .pipe(csscomb())
         .pipe(gulp.dest(config.css.DEST)) //non-minified CSS
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(rename({ extname: '.min.css' }))
