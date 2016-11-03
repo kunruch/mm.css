c88583d54ac0203d4d989e23f7404a84fa58fd4e
Kanishk Kunal <kanishk.kunal@hotmail.com>
2016-11-03 14:17:50 +0530

    Auto update of minified gzipped size of css

---
+++ web/data/stats.js
@@ -0,0 +1,25 @@
+fs   = require('fs');
+path = require('path');
+var gzipSize = require('gzip-size');
+
+exports.data = function() {
+  var data = {
+    size: 0
+  }
+  
+  // get dist path
+  var distPath = path.join(__dirname, '../..', 'dist', 'mm.min.css')
+
+  try {
+    var string = fs.readFileSync(distPath, 'utf8');
+ 
+    // console.log((string.length / 1024) + "KB");
+
+    data.size = (gzipSize.sync(string) / 1024).toFixed(2);
+  } catch (e) {
+    console.log(e);
+  }
+
+  // console.log("DATA: " + JSON.stringify(data))
+  return data
+}
