431679b81f48486950c19f02de192ad34d82490a
Kanishk Kunal <kanishk.kunal@hotmail.com>
2018-01-11 20:08:42 +0530

    Updating dist

--- web/data/stats.js
+++ web/data/stats.js
@@ -6,13 +6,13 @@ exports.data = function() {
   var data = {
     size: 0
   }
-  
+
   // get dist path
   var distPath = path.join(__dirname, '../..', 'dist', 'mm.min.css')
 
   try {
     var string = fs.readFileSync(distPath, 'utf8');
- 
+
     // console.log((string.length / 1024) + "KB");
 
     data.size = (gzipSize.sync(string) / 1024).toFixed(2);
